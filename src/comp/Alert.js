52f124d1ca826c8bd71ef483546bf896781faa7a
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2016-07-20 16:04:22 +0200

    Add method reportError() to Alert mixin

--- src/comp/Alert.js
+++ src/comp/Alert.js
@@ -7,18 +7,37 @@ function alert(comp, level, content) {
 
 export default {
     methods: {
+        /*
+         * Sends an error event.
+         */
+        reportError(error) {
+            this.$dispatch("error-event", error)
+        },
+
+        /*
+         * Sends an alert events with level "danger".
+         */
         alertError(content) {
             alert(this, "danger", content)
         },
 
+        /*
+         * Sends an alert events with level "warning".
+         */
         alertWarning(content) {
             alert(this, "warning", content)
         },
 
+        /*
+         * Sends an alert events with level "info".
+         */
         alertInfo(content) {
             alert(this, "info", content)
         },
 
+        /*
+         * Sends an alert events with level "success".
+         */
         alertSuccess(content) {
             alert(this, "success", content)
         }
