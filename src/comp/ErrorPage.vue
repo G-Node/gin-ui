bae96a744b1f40e771e2051ee2d4e7592655ebfc
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2016-07-20 16:01:32 +0200

    Page showing an error message

---
+++ src/comp/ErrorPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ error_heading }}</h1>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: {
+            error: { required: true }
+        },
+
+        computed: {
+            error_heading: {
+                get() {
+                    if (this.error.hasOwnProperty("message")) {
+                        return this.error.message
+                    } else {
+                        return this.error
+                    }
+                }
+            }
+        }
+    }
+</script>
