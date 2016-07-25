0077a1a713552811caa1147cc25068750958cd4d
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2016-07-25 11:23:40 +0200

    data: remove unused function randStr

--- src/data.js
+++ src/data.js
@@ -21,15 +21,6 @@ function randElem(array) {
     }
 }
 
-function randStr(length) {
-    let letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-    let res = ""
-    for (let i = 0; i < length; i++) {
-        res = res + randElem(letters)
-    }
-    return res
-}
-
 function randDirs() {
     const max_depth = 4
     const max_files = 10
