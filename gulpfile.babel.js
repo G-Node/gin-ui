59f5b3119165701ac54df633bb4bef4dfaa15f01
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2016-07-25 11:37:18 +0200

    Move files to sub directories

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,14 @@
 import gulp from "gulp"
+import copy from "gulp-copy"
 import uglify from "gulp-uglify"
 import sourcemaps from "gulp-sourcemaps"
 import named from "vinyl-named"
 import connect from "gulp-connect"
 import webpack from "webpack-stream"
+
+/*
+ * Build configuration
+ */
 
 const webpack_config = {
     module: {
@@ -19,29 +24,52 @@ const webpack_config = {
     devtool: "source-map"
 }
 
+
+const copy_files = [
+    "src/index.html"
+]
+
+const connect_config = {
+    port: 8080,
+    livereload: false,
+    root: "build",
+    index: "index.html",
+    fallback: "build/index.html"
+}
+
+/*
+ * Define tasks
+ */
+
 gulp.task("vue", () => {
-    return gulp.src(["src/main.js"])
+    return gulp.src(["src/js/main.js"])
         .pipe(named())
         .pipe(webpack(webpack_config))
-        .pipe(gulp.dest("build/"))
+        .pipe(gulp.dest("build/js/"))
         .pipe(connect.reload())
 })
 
 gulp.task("vue-minify", ["vue"], () => {
-    return gulp.src(["build/main.js"])
+    return gulp.src(["build/js/main.js"])
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify({ mangle: false }))
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("build/"))
+        .pipe(gulp.dest("build/js/"))
         .pipe(connect.reload())
 })
 
-gulp.task("watch", ["vue"], () => {
-    return gulp.watch("src/**/*", ["vue"])
+gulp.task("copy", () => {
+    return gulp.src(copy_files)
+        .pipe(copy("build", { prefix: 1 }))
+        .pipe(connect.reload())
+})
+
+gulp.task("watch", ["vue", "copy"], () => {
+    return gulp.watch("src/**/*", ["vue", "copy"])
 })
 
 gulp.task("dev", ["watch"], () => {
-    return connect.server({ port: 8080, livereload: true, fallback: "index.html" })
+    return connect.server(connect_config)
 })
 
-gulp.task("default", ["vue-minify"])
+gulp.task("default", ["vue-minify", "copy"])
