ec6c90d66c3ff451038a29c0219740a40b30d2a1
Achilleas Koutsou <achilleas.k@gmail.com>
2016-08-02 18:31:00 +0200

    Use search component

--- src/js/comp/repo/PublicRepos.vue
+++ src/js/comp/repo/PublicRepos.vue
@@ -2,12 +2,7 @@
     <div>
         <h2>Public Repositories</h2>
         <hr />
-        <div class="form-inline row">
-            <div class="form-group col-sm-9 col-sm-offset-1">
-                <label class="sr-only" for="search">Search</label>
-                <input type="text" class="form-control" id="search" placeholder="Search Text" v-model="search_text" style="width: 100%" @keypress.enter="search" debounce="300">
-            </div>
-        </div>
+        <repo-search v-bind:repositories.sync="repositories"></repo-search>
         <hr />
         <ul class="list-unstyled">
             <li v-for="repo in repositories">
@@ -25,35 +20,16 @@
 </template>
 
 <script type="text/ecmascript-6">
-    export default{
-        data(){
+    import Vue from "vue"
+
+    import RepoSearch from "../repo/RepoSearch.vue"
+
+    Vue.component("repo-search", RepoSearch)
+
+    export default {
+        data() {
             return {
-                search_text: null,
                 repositories: null
-            }
-        },
-
-        ready() {
-            this.search()
-        },
-
-        methods: {
-            search() {
-                const promise = api.repos.listPublic(this.search_text)
-                promise.then(
-                    (repos) => {
-                        this.repositories = repos
-                    },
-                    (error) => {
-                        this.reportError(error)
-                    }
-                )
-            }
-        },
-
-        watch: {
-            "search_text": function() {
-                this.search()
             }
         }
     }
