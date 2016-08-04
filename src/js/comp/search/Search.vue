d3a9a4807289bcaeb34f4395e780f5ba902b65e1
Achilleas Koutsou <achilleas.k@gmail.com>
2016-08-04 10:56:54 +0200

    Fix error handling in search form

--- src/js/comp/search/Search.vue
+++ src/js/comp/search/Search.vue
@@ -1,34 +1,39 @@
 <template>
-    <h2>Search</h2>
-    <hr />
-    <div class="form-inline row">
-        <div class="form-group col-sm-9 col-sm-offset-1">
-            <label class="sr-only" for="search">Search</label>
-            <input type="text" class="form-control" id="search" placeholder="Search Text" v-model="search_text" style="width: 100%" @keypress.enter="search" debounce="300">
-        </div>
-    </div>
-    <hr />
-
     <div>
-        <div v-if="search_text">
-            <ul class="nav nav-tabs">
-                <li role="presentation" :class="{ 'active': $route.name === 'search-repos' }">
-                    <a v-link="{ name: 'search-repos' }">Repositories ({{ repositories.length }})</a>
-                </li>
-                <li role="presentation" :class="{ 'active': $route.name === 'search-users' }">
-                    <a v-link="{ name: 'search-users' }">Users ({{ users.length }})</a>
-                </li>
-            </ul>
-
-            <router-view v-bind:repositories.sync="repositories" v-bind:users.sync="users"></router-view>
+        <h2>Search</h2>
+        <hr />
+        <div class="form-inline row">
+            <div class="form-group col-sm-9 col-sm-offset-1">
+                <label class="sr-only" for="search">Search</label>
+                <input type="text" class="form-control" id="search" placeholder="Search Text" v-model="search_text" style="width: 100%" @keypress.enter="search" debounce="300">
+            </div>
         </div>
-        <div v-if="!search_text">
-            Search for public repositories or users
+        <hr />
+
+        <div>
+            <div v-if="search_text">
+                <ul class="nav nav-tabs">
+                    <li role="presentation" :class="{ 'active': $route.name === 'search-repos' }">
+                        <a v-link="{ name: 'search-repos' }">Repositories ({{ repositories.length }})</a>
+                    </li>
+                    <li role="presentation" :class="{ 'active': $route.name === 'search-users' }">
+                        <a v-link="{ name: 'search-users' }">Users ({{ users.length }})</a>
+                    </li>
+                </ul>
+
+                <router-view v-bind:repositories.sync="repositories" v-bind:users.sync="users"></router-view>
+            </div>
+            <div v-if="!search_text">
+                Search for public repositories or users
+            </div>
         </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+
+    import Alert from "../Alert.js"
+
     export default {
         props: {
             search_text: null
@@ -53,18 +58,12 @@
                 repo_search.then(
                         (repos) => {
                             this.repositories = repos
-                        },
-                        (error) => {
-                            this.reportError(error)
                         }
                 )
 
                 user_search.then(
                         (users) => {
                             this.users = users
-                        },
-                        (error) => {
-                            this.reportError(error)
                         }
                 )
 
@@ -82,10 +81,15 @@
                                     name: tab
                                 })
                             }
+                        },
+                        (error) => {
+                            this.reportError(error)
                         }
                 )
             }
         },
+
+        mixins: [Alert],
 
         watch: {
             "search_text": function() {
