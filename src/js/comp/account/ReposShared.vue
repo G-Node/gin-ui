d1da34e3fdb9295145a2d30c4bc33b40a736f37b
M. Sonntag <michael.p.sonntag@gmail.com>
2016-10-13 19:15:43 +0200

    repo: List shared repos from repo service

--- src/js/comp/account/ReposShared.vue
+++ src/js/comp/account/ReposShared.vue
@@ -14,6 +14,22 @@
                 </div>
             </li>
         </ul>
+        <hr/>
+        <ul class="list-unstyled">
+            <li v-for="repo in repos_shared">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                            {{ repo.Owner }}/{{ repo.Name }}
+                    </div>
+                    <div class="panel-body">
+                        Head: {{ repo.Head }} <br/>
+                        Description: {{ repo.Description }} <br/>
+                        Public: {{ repo.Visibility }}
+                    </div>
+                </div>
+            </li>
+        </ul>
+
     </div>
 </template>
 
@@ -23,7 +39,8 @@
     export default {
         data() {
             return {
-                repositories: null
+                repositories: null,
+                repos_shared: null
             }
         },
 
@@ -41,7 +58,7 @@
         methods: {
             update(accounts) {
                 const login_name = accounts.account ? accounts.account.login : null
-                const promise  = api.repos.listShared(accounts.owner.login, login_name)
+                const promise  = api.repos.listSharedOld(accounts.owner.login, login_name)
                 promise.then(
                     (repos) => {
                         this.repositories = repos
@@ -50,6 +67,17 @@
                         this.repositories = null
                         this.reportError(error)
                     }
+                )
+
+                const repos_shared = api.repos.listShared()
+                repos_shared.then(
+                        (repos) => {
+                            this.repos_shared = repos
+                        },
+                        (error) => {
+                            this.repos_shared = null
+                            this.reportError(error)
+                        }
                 )
             }
         }
