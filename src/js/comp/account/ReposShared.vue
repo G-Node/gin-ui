f174182484fdf58778f8d2916c1462feea105f88
M. Sonntag <michael.p.sonntag@gmail.com>
2016-10-13 19:18:27 +0200

    repo: cleanup ReposShared

--- src/js/comp/account/ReposShared.vue
+++ src/js/comp/account/ReposShared.vue
@@ -1,25 +1,10 @@
 <template>
     <div>
         <ul class="list-unstyled">
-            <li v-for="repo in repositories">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <router-link :to="{ name: 'repository-info', params: { username: repo.owner, repository: repo.name}}">
-                            {{ repo.owner }}/{{ repo.name }}
-                        </router-link>
-                    </div>
-                    <div class="panel-body">
-                        {{ repo.description }}
-                    </div>
-                </div>
-            </li>
-        </ul>
-        <hr/>
-        <ul class="list-unstyled">
             <li v-for="repo in repos_shared">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                            {{ repo.Owner }}/{{ repo.Name }}
+                        {{ repo.Owner }}/{{ repo.Name }}
                     </div>
                     <div class="panel-body">
                         Head: {{ repo.Head }} <br/>
@@ -29,7 +14,6 @@
                 </div>
             </li>
         </ul>
-
     </div>
 </template>
 
@@ -39,36 +23,22 @@
     export default {
         data() {
             return {
-                repositories: null,
                 repos_shared: null
             }
         },
 
         mounted() {
-            this.update({ owner: this.owner, account: this.account })
+            this.update()
         },
 
         props: {
-            account: { required: true },
-            owner: { required: true }
+            account: { required: true }
         },
 
         mixins: [ Alert ],
 
         methods: {
-            update(accounts) {
-                const login_name = accounts.account ? accounts.account.login : null
-                const promise  = api.repos.listSharedOld(accounts.owner.login, login_name)
-                promise.then(
-                    (repos) => {
-                        this.repositories = repos
-                    },
-                    (error) => {
-                        this.repositories = null
-                        this.reportError(error)
-                    }
-                )
-
+            update() {
                 const repos_shared = api.repos.listShared()
                 repos_shared.then(
                         (repos) => {
