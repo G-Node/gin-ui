ac1773c4ff40996bea7b1cdffc6567b5448afcd0
M. Sonntag <michael.p.sonntag@gmail.com>
2016-10-13 19:04:18 +0200

    repo: cleanup ReposOwn

--- src/js/comp/account/ReposOwn.vue
+++ src/js/comp/account/ReposOwn.vue
@@ -1,21 +1,6 @@
 <template>
     <div>
         <ul class="list-unstyled">
-            <li v-for="repo in repositories">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <router-link :to="{ name: 'repository-info', params: { username: repo.owner, repository: repo.name}}">
-                            {{ repo.owner }}/{{ repo.name }}
-                        </router-link>
-                    </div>
-                    <div class="panel-body">
-                        {{ repo.description }}
-                    </div>
-                </div>
-            </li>
-        </ul>
-        <hr/>
-        <ul class="list-unstyled">
             <li v-for="repo in own_repos">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -38,36 +23,22 @@
     export default {
         data() {
             return {
-                repositories: null,
                 own_repos: null
             }
         },
 
         mounted() {
-            this.update({ owner: this.owner, account: this.account })
+            this.update()
         },
 
         props: {
-            account: { required: true },
-            owner: { required: true }
+            account: { required: true }
         },
 
         mixins: [ Alert ],
 
         methods: {
-            update(accounts) {
-                const login_name = accounts.account ? accounts.account.login : null
-                const promise  = api.repos.listOwnOld(accounts.owner.login, login_name)
-                promise.then(
-                    (repos) => {
-                        this.repositories = repos
-                    },
-                    (error) => {
-                        this.repositories = null
-                        this.reportError(error)
-                    }
-                )
-
+            update() {
                 const getOwn = api.repos.listOwn(this.account.login)
                 getOwn.then(
                         (repos) => {
