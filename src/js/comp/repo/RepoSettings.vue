8d3766bed5a94a3811a97740ab59d4740eb00aaf
M. Sonntag <michael.p.sonntag@gmail.com>
2017-01-13 21:24:40 +0100

    [comp/RepoSettings] collab permission selection
    Closes #53

--- src/js/comp/repo/RepoSettings.vue
+++ src/js/comp/repo/RepoSettings.vue
@@ -47,6 +47,14 @@
                                         <tr v-for="text in form.shared">
                                             <td>{{ text.User }}</td>
                                             <td class="text-right">
+                                                <span v-for="level in permissions">
+                                                    <button v-if="text.AccessLevel === level" class="btn btn-success btn-xs"
+                                                            @click="updateCollaborator(text.User, level)">{{ level }}</button>
+                                                    <button v-if="text.AccessLevel !== level" class="btn btn-xs"
+                                                            @click="updateCollaborator(text.User, level)">{{ level }}</button>
+                                                </span>
+                                            </td>
+                                            <td class="text-right">
                                                 <button class="btn btn-danger btn-xs" @click="removeShare(text.User)">remove</button>
                                             </td>
                                         </tr>
