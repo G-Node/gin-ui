be81f131c3585875b0e799a67f1fa603bc08accb
M. Sonntag <michael.p.sonntag@gmail.com>
2017-01-20 13:14:10 +0100

    [comp/Index] add main page text

--- src/js/comp/Index.vue
+++ src/js/comp/Index.vue
@@ -1,5 +1,92 @@
 <template>
     <div>
-        <h1>Start Page</h1>
+        <h1>Welcome to the <strong>GIN</strong> (<strong>G</strong>-Node <strong>IN</strong>frastructure)
+            web services!</h1>
+        <hr />
+        <p>This service aims to provide a free, secure and community driven alternative to other
+            proprietary cloud services.</p>
+
+        <p>We therefore provide free data synchronisation, hosting, publishing and sharing for
+            scientists to promote data sharing and collaboration within the scientific community!</p>
+
+        <p>Therefore this service will always be free and Open Source - you can even setup your
+            own in-house instance of this service.</p>
+
+        <p>We also provide a free publishing service for data by issuing DOIs to repositories.</p>
+
+        <h2>Features:</h2>
+
+        <ul>
+            <li>Choose how you want to use our service
+                <ul>
+                    <li>register with the GIN services and use the provided infrastructure</li>
+                    <li>set up and host your own in-house instance -
+                        our software is open source, you can use it for free</li>
+                </ul>
+            </li>
+            <li>Manage your data from anywhere
+                <ul>
+                    <li>upload your data on a repository based structure: you can create
+                        as many repositories as you like.</li>
+                    <li>access your data from anywhere: once the data is at the main repository
+                        service you can access your data from anywhere you like.</li>
+                    <li>synchronize your data: you can download complete or partial repositories
+                        on any workplace you like, work on them locally and upload the changes back
+                        to the main repository.</li>
+                    <li>version your data: when changing your files and uploading them to the server,
+                        the history is automatically kept, you can always go back to a previous version.</li>
+                </ul></li>
+            <li>Share your data
+                <ul>
+                    <li>make your data public: if you want to make your data accessible to the world,
+                        just make your repository publicly available. The data will be accessible but
+                        only you will be able to change it.</li>
+                    <li>share your data with collaborators: you can also share your public or private
+                        repositories with other users of the GIN service making it easy to jointly work on a project.</li>
+                </ul>
+            </li>
+            <li>Publish your data (DOI)
+                <ul>
+                    <li>when you are hosting your data on the GIN server, we give you the option
+                        to publish any of your repositories. We will issue a DOI reliably hosting
+                        your data for your publications. </li>
+                </ul>
+            </li>
+            <li>Enhanced search of your repositories
+                <ul>
+                    <li>By indexing the repository contents it's easy to find the files you are looking for</li>
+                    <li>When using the <a href="https://github.com/G-Node/nix">NIX</a> (Neuroscience Exchange format)
+                        for scientific data and metadata, even the contents of these files will
+                        be indexed and searchable making it easy for you to identify even the data
+                        you are looking for.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h2>How to use the GIN services</h2>
+
+        <p>If you want to use our services</p>
+        <ul>
+            <li><a @click="window.api.register()">register</a> an account with us.</li>
+            <li>download the command line client <a href="https://github.com/G-Node/gin-cli">gin-cli</a>.</li>
+            <li>upload your data using gin-cli.</li>
+            <li>access your data via the web services or download them on another machine using gin-cli.</li>
+        </ul>
+
+        <h2>Set up your own in-house GIN service</h2>
+
+        <p>All of the GIN services are Open Source software, if you want to host your data solely
+            in-house on your own servers, this can easily be done and for free as well!<br/>
+            You can find the complete source code and documentation on github: </p>
+
+        <ul>
+            <li><a href="https://github.com/G-Node/gin-ui">gin-ui</a>: The web interface for the GIN services.</li>
+            <li><a href="https://github.com/G-Node/gin-cli">gin-cli</a>: The command line client.</li>
+            <li><a href="https://github.com/G-Node/gin-repo">gin-repo</a>: The repository server.</li>
+            <li><a href="https://github.com/G-Node/gin-auth">gin-auth</a>: The authentication server.</li>
+        </ul>
+
+        <p>If you need any help setting up your own service, you can <a href="#">contact us</a>. [TBD]</p>
+
     </div>
 </template>
