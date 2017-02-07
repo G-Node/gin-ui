debdb25e303caaa1eb2126d789c0b90343ed2b46
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-07 17:45:31 +0100

    [comp/RepoDOI] create file

---
+++ src/js/comp/repo/RepoDOI.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Create DOI from your current repository status.
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import { event } from "../../events.js"
+    import Alert from "../Alert.js"
+
+    event.init()
+
+    export default {
+        props: {
+            repository: { required: true },
+        },
+
+        mixins: [ Alert ],
+
+    }
+</script>
