4571eab6b04038de3f7fa0722d62a458d3613773
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-08 20:55:23 +0100

    [comp/RepoDOI] add update method

--- src/js/comp/repo/RepoDOI.vue
+++ src/js/comp/repo/RepoDOI.vue
@@ -11,6 +11,13 @@
     event.init()
 
     export default {
+        data() {
+            return {
+                can_doi: null,
+                doi_file: null,
+            }
+        },
+
         props: {
             repository: { required: true },
             account: { required: true},
@@ -18,15 +25,52 @@
 
         mixins: [ Alert ],
 
+        methods: {
+            update(params) {
+                this.can_doi = false
+                this.doi_file = "README.md"
+
+                var acc = (this.account !== undefined && this.account !== null)
+                var rep = this.repository.Public
+
+                if (acc && rep) {
+                    console.log("[RepoDOI] basic checks passed, fetch repo root")
+                    const promise_root = api.repos.getDirectorySection(
+                            params.username,
+                            params.repository,
+                            "master", "")
+                    promise_root.then(
+                            (root) => {
+                                console.log("[RepoDOI] repo fetched, find required file")
+                                var entries = root.entries
+                                for (var entry of entries) {
+                                    if (entry.name && entry.name === this.doi_file) {
+                                        console.log("[RepoDOI] required file present")
+                                        this.can_doi = true
+                                        break
+                                    }
+                                }
+                            },
+                            (error) => {
+                                console.log("[RepoDOI] error while trying to fetch repo")
+                                console.log(error)
+                            }
+                    )
+                }
+            }
+        },
+
         // Required due to race condition with App.vue, where "account" is reloaded,
         // when the route is changed directly.
         watch: {
             "account": function () {
                 console.log("[RepoDOI] account has changed, update")
+                this.update(this.$route.params)
             },
 
             "$route.params": function () {
                 console.log("[RepoDOI] route has changed, update")
+                this.update(this.$route.params)
             }
         }
     }
