6f8e58c00a3fc69ff3cf99da31b74e3eeeeb0779
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-09 18:23:56 +0100

    license consolidation

--- src/js/comp/search/Search.vue
+++ src/js/comp/search/Search.vue
@@ -1,3 +1,13 @@
+<!--
+    Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted under the terms of the BSD License. See
+    LICENSE file in the root of the Project.
+-->
+
 <template>
     <div>
         <h2>Search</h2>
