2b1da6d8a2059cfbca3e1caac1dd0518bf48c664
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-13 19:50:20 +0100

    [utils.js] add stateHash function

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,3 +5,16 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted under the terms of the BSD License. See
 // LICENSE file in the root of the Project.
+
+import md5 from "md5"
+
+export function stateHash(client_id, user_agent) {
+    var d = new Date()
+    return md5(
+        d.getMonth().toString().concat(
+            d.getDate().toString(),
+            client_id,
+            user_agent
+        )
+    )
+}
