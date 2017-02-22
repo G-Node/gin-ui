f3d33198e87d4afe5c54bf83f690c4939c0ac54f
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-22 16:38:54 +0100

    [comp/Contact] add content

--- src/js/comp/info/Contact.vue
+++ src/js/comp/info/Contact.vue
@@ -10,10 +10,36 @@
 
 <template>
     <div class="container">
-        Contact
+        <p>
+            G-Node (German Neuroinformatics Node)
+            <br />
+            Biozentrum
+            Ludwig-Maximilians-Universität München
+            Großhaderner Straße 2
+            82152 Martinsried-Planegg
+            Germany
+            <br />
+            Phone: +49 (0)89 2180 74810
+            Fax: +49 89 2180 74803
+            eMail: <a :href="mailto">{{ contact }}</a>
+            <br />
+            <a href="https://www.google.de/maps/place/G-Node/@48.109108,11.459057,15z/data=!4m5!3m4!1s0x0:0x1d6841c041030d33!8m2!3d48.109108!4d11.459057">
+                G-Node location
+            </a>
+            <br />
+        </p>
     </div>
 </template>
 
 <script>
-
+    export default {
+        computed: {
+            mailto: function() {
+                return "mailto:"+ window.api.config.contact_email
+            },
+            contact: function() {
+                return window.api.config.contact_email
+            },
+        },
+    }
 </script>
