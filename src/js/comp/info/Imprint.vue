222fdf9e026f8356c4537f566bda3e26f754d66e
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-22 16:42:38 +0100

    [comp/Imprint] add page

---
+++ src/js/comp/info/Imprint.vue
@@ -0,0 +1,19 @@
+<!--
+    Copyright (c) 2017, German Neuroinformatics Node (G-Node)
+
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted under the terms of the BSD License. See
+    LICENSE file in the root of the Project.
+-->
+
+<template>
+    <div class="container">
+        Imprint
+    </div>
+</template>
+
+<script>
+
+</script>
