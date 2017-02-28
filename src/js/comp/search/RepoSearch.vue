d2b499c0de712954a2aa9ed2cd4de7f08f58439d
M. Sonntag <michael.p.sonntag@gmail.com>
2017-02-28 21:57:42 +0100

    [comp/RepoSearch] add owner full name to repo list
    Closes #83

--- src/js/comp/search/RepoSearch.vue
+++ src/js/comp/search/RepoSearch.vue
@@ -11,7 +11,7 @@
 <template>
     <div>
         <ul class="list-unstyled">
-            <li v-for="repo in public_repo">
+            <li v-for="repo in repos_modified">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <router-link :to="{ name: 'repository-info',
@@ -20,7 +20,10 @@
                         </router-link>
                     </div>
                     <div class="panel-body">
+                        Owner {{ repo.FullName }}
+                        <br/><br/>
                         {{ repo.Description }}
+                        <span v-if="!repo.Description">No description for this repository.</span>
                     </div>
                 </div>
             </li>
@@ -30,8 +33,47 @@
 
 <script type="text/ecmascript-6">
     export default {
+        data() {
+            return {
+                repos_modified: null
+            }
+        },
+
         props: {
             public_repo: {required: true}
+        },
+
+        mounted() {
+            this.augment()
+        },
+
+        methods: {
+            augment() {
+                if (this.public_repo !== null && this.public_repo !== undefined) {
+                    const user_search = api.accounts.search()
+                    user_search.then(
+                            (u) => {
+                                var names_map = new Map()
+                                for (var j = 0; j < u.length; j++) {
+                                    names_map.set(u[j].login, u[j].first_name+" "+u[j].last_name)
+                                }
+
+                                this.repos_modified = []
+                                for (var i = 0; i < this.public_repo.length; i++) {
+                                    var el = Object.assign({}, this.public_repo[i],
+                                            { FullName: names_map.get(this.public_repo[i].Owner) })
+                                    this.repos_modified.push(el)
+                                }
+                            }
+                    )
+                }
+            }
+        },
+
+        watch: {
+            "public_repo": function() {
+                this.augment()
+            }
         }
     }
 </script>
