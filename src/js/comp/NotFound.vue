72dbb1d83244ba7a708454f9044070ee13606230
M. Sonntag <michael.p.sonntag@gmail.com>
2017-03-29 19:54:05 +0200

    [comp/NotFound] add 404 page

---
+++ src/js/comp/NotFound.vue
@@ -0,0 +1,22 @@
+<!--
+    Copyright (c) 2017, German Neuroinformatics Node (G-Node)
+
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted under the terms of the BSD License. See
+    LICENSE file in the root of the Project.
+-->
+
+<template>
+    <div>
+        <h1>Page not found</h1>
+        <hr>
+
+        <p>We looked hard, but could found nothing at this location.
+            The Lag Biest must have hidden what you were looking for.</p>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
