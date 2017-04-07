33db095eed559ee99e3e6c7cf4b93a816ac08c84
M. Sonntag <michael.p.sonntag@gmail.com>
2017-04-08 00:04:24 +0200

    [comp/ReposShared] add pagination index

--- src/js/comp/account/ReposShared.vue
+++ src/js/comp/account/ReposShared.vue
@@ -13,8 +13,12 @@
         <!-- pagination -->
         <nav v-if="repos_modified">
             <ul class="pager">
-                <li class="previous"><a @click="prev">Previous</a></li>
-                <li class="next"><a @click="next">Next</a></li>
+                <li class="previous"><a @click="prev">
+                    Previous <span v-if="pidx !== null">({{ pidx }})</span>
+                </a></li>
+                <li class="next"><a @click="next">
+                    Next <span v-if="nidx !== null">({{ nidx }})</span>
+                </a></li>
             </ul>
         </nav>
 
@@ -64,7 +68,9 @@
             return {
                 repos_modified: null,
                 repos_displayed: null,
-                idx: 0
+                idx: 0,
+                pidx: null,
+                nidx: null
             }
         },
 
@@ -114,6 +120,8 @@
                                             var len = n_displayed > this.repos_modified.length ?
                                                     this.repos_modified.length : n_displayed
                                             this.repos_displayed = this.repos_modified.slice(this.idx, len)
+
+                                            this.updatePagerIndex()
                                         },
                                         (error) => {
                                             console.log(ll +" error fetching user for shared repos")
@@ -131,12 +139,19 @@
                 let prev = pagerPrevious(this.repos_modified, this.idx, n_displayed)
                 this.repos_displayed = prev.arr
                 this.idx = prev.index
+                this.updatePagerIndex()
             },
 
             next() {
                 let nxt = pagerNext(this.repos_modified, this.idx, n_displayed)
                 this.repos_displayed = nxt.arr
                 this.idx = nxt.index
+                this.updatePagerIndex()
+            },
+
+            updatePagerIndex() {
+                this.pidx = Math.floor(this.idx / n_displayed)
+                this.nidx = Math.floor((this.repos_modified.length - this.idx) / n_displayed)
             }
         },
 
