3b13117c2629e665a96c92e63d15361c0b9fb119
M. Sonntag <michael.p.sonntag@gmail.com>
2017-04-18 19:33:21 +0200

    [log] add basic logger

---
+++ src/js/log.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2017, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * Basic logging class.
+ */
+export default class Logger {
+    /**
+     * print switches between different log levels and prints messages accordingly.
+     *
+     * @param lvl {string}          level at which the message is supposed to be logged.
+     * @param message {object}      message to be logged.
+     */
+    print(lvl, message) {
+        switch (lvl) {
+        case "Debug":
+            console.log(message)
+            break
+        case "Err":
+            console.error(message)
+            break
+        default:
+            console.log(message)
+        }
+    }
+}
